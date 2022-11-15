a93d1fdf344e20ad7a09ed4f619939f28cd5f054
jinshuang <niu.jins@northeastern.edu>
2022-11-14 16:32:46 -0800

    project setup

---
+++ src/main/java/com/example/advanced_webapp/AdvancedWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.advanced_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedWebappApplication.class, args);
+        System.out.println("worked");
+    }
+}
